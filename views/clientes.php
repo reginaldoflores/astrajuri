2c4177da16ea2df151ab01621e5a6291829360e7
Reginaldo <regin@ASUS-X510U>
2018-08-28 11:00:03 -0300

    Alteração na tela de cadastro.

--- views/clientes.php
+++ views/clientes.php
@@ -40,7 +40,6 @@
                                     </div>
                                     
                                     <div id="pessoaJuridica">
-                                        
                                         <div class="item form-group">
                                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="insc_municipal">Inscrição Municipal: <span class="required">*</span>
                                             </label>
@@ -57,10 +56,10 @@
                                             </div>
                                         </div>
                                     </div>
+                                    
                                     <div id="pessoaFisica">
-                                        
                                         <div class="item form-group">
-                                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="rg">RG
+                                            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="rg">RG:
                                             </label>
                                             <div class="col-md-3 col-sm-6 col-xs-12">
                                                 <input type="text" id="rg" name="rg" size="12" class="form-control col-md-7 col-xs-12">
@@ -208,7 +207,7 @@
                                     <div class="ln_solid"></div>
                                     <div class="form-group">
                                         <div class="col-md-6 col-md-offset-3">
-                                            <button id="send" type="submit" class="btn btn-primary">Atualizar</button>
+                                            <button id="send" type="submit" class="btn btn-primary">Incluir</button>
                                             <button type="reset" class="btn btn-default">Cancelar</button>
                                             <span id="vemAqui"></span>
                                         </div>
